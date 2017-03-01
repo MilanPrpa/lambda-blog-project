88003b7efea756305a69a341da7610295c2a0709
MilanPrpa <milanprpa25@gmail.com>
2017-02-28 19:17:52 -0700

    updated lambda project database

--- my_blog.py
+++ my_blog.py
@@ -2,42 +2,38 @@ from flask import Flask, render_template, request
 import sqlite3
 app = Flask(__name__)
 
+
 connection = sqlite3.connect('database.db')
 print('DATABASE CONNECTED')
 
-connection.execute('CREATE TABLE IF NOT EXISTS posts(title TEXT, post TEXT)')
+connection.execute('CREATE TABLE IF NOT EXISTS posts (title TEXT, post TEXT)')
 print('TABLE CREATED')
 
 connection.close()
 
 
 @app.route('/')
-def homePage():
-    return 'Welcome!!'
+def home():
+    return 'Dobro Dosli!'
 
 @app.route('/new')
 def newPost():
     return render_template('home.html')
 
-@app.route('/addrecord', methods = ['POST'])
-def addRecord():
+@app.route('/addrecord', methods=['POST'])
+def addrecord():
     connection = sqlite3.connect('database.db')
     cursor = connection.cursor()
 
     try:
         title = request.form['title']
         post = request.form['post']
-        cursor.execute('INSERT INTO posts(title, post) VALUES (?, ?)', (title, post))
+        cursor.execute('INSERT INTO posts (title, post) VALUES (?, ?)' , (title, post))
         connection.commit()
-        message = 'it worked'
+        message = 'SUCCESS!!!'
     except:
         connection.rollback()
-        message = 'error'
+        message = 'ERROR ERROR'
     finally:
         return render_template('result.html', message = message)
-        connection.close()
-
-
-
-
-    app.run(debug = True)
+        conneciton.close()
